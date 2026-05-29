f49c04f8e54e25cfb33025bdcd786100e7526fac
Sanda <wwematic@yahoo.com>
2026-05-29 18:45:44 +1000

    small changes for accessibility

--- cos30043-groupAssignment/src/App.vue
+++ cos30043-groupAssignment/src/App.vue
@@ -21,8 +21,8 @@ onMounted(() => {
   <div>
     <div class="guest-nav">
       
-      <img v-if="isOpen" @click="isOpen = !isOpen" id="menu" :src="crossIcon">
-      <img v-else @click="isOpen = !isOpen" id="menu" :src="hamburgerIcon">
+      <img v-if="isOpen" @click="isOpen = !isOpen" id="menu" :src="crossIcon" alt="Navigation X">
+      <img v-else @click="isOpen = !isOpen" id="menu" :src="hamburgerIcon" alt="Navigation Hamburger">
       
       <transition name="slide">
         <aside v-if="isOpen" class="nav-links">
@@ -57,7 +57,10 @@ onMounted(() => {
       </transition>
     </div>
 
-    <router-view />
+    <main>
+      <router-view />
+    </main>
+
     
   </div>
 
@@ -66,22 +69,22 @@ onMounted(() => {
       <div class="container">
         <div class="row">
           <div class="col-lg-4 col-12 d-flex justify-content-center justify-content-lg-start">
-            <img id="logo" :src="logo">
+            <img id="logo" :src="logo" alt="Retrospect Logo">
           </div>
           <div class="col-lg-4 col-6">
-            <h3 id="footer-header">Get to Know Us!</h3>
+            <h1 id="footer-header">Get to Know Us!</h1>
             <nav>
-              <h6><router-link to="/about-us">About Us</router-link></h6>
-              <h6><router-link to="/contact-us">Contact Us</router-link></h6>
-              <h6><router-link to="/policies">Policies</router-link></h6>
+              <h2><router-link to="/about-us">About Us</router-link></h2>
+              <h2><router-link to="/contact-us">Contact Us</router-link></h2>
+              <h2><router-link to="/policies">Policies</router-link></h2>
             </nav>
           </div>
           <div class="col-lg-4 col-6">
-            <h3 id="footer-header">Accounts</h3>
+            <h1 id="footer-header">Accounts</h1>
             <nav>
-              <h6><router-link to="/account">Your Account</router-link></h6>
-              <h6><router-link to="/login">Login</router-link></h6>
-              <h6><router-link to="/sign-up">Create an Account</router-link></h6>
+              <h2><router-link to="/account">Your Account</router-link></h2>
+              <h2><router-link to="/login">Login</router-link></h2>
+              <h2><router-link to="/sign-up">Create an Account</router-link></h2>
             </nav>
           </div>
         </div>
